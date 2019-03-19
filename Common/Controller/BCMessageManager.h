2d1d3770bd9feed008f79491fee370d78c75d942
梅杰 <mj19961012@outlook.com>
2019-03-19 22:57:11 +0800

    同步

--- Common/Controller/BCMessageManager.h
+++ Common/Controller/BCMessageManager.h
@@ -8,7 +8,7 @@ public :
     BCMessageManager();
     ~BCMessageManager();
     static BCMessageManager * getInstance();
-    void BCLoginHandle();
+    void BCLoginHandle(std::string username,std::string password);
 private:
     user_info current_user;
 };
